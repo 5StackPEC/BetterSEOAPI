373be6f8d21beb5e6792f4e94e526d4fb361c0ef
Cristian Cazares <cristian.cazares@atratopago.com>
2024-05-29 16:49:06 -0600

    feat(fileUpload): added file saving

---
+++ controllers/model.py
@@ -0,0 +1,8 @@
+from fastapi import UploadFile
+
+class Model:
+    def __init__(self, image):
+        self.image = image
+    
+    
+    
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
+from pathlib import Path
+import shutil
+import os
+from typing import Annotated
 
-from uvicorn import run
+
 
 app = FastAPI()
 
@@ -20,6 +23,7 @@ app.add_middleware(
 )
 
 
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to Better SEO API!"}
@@ -32,5 +36,12 @@ async def get_score(file: Annotated[bytes, File()]):
 
 @app.post("/score_alt")
 async def get_score_alt(file: UploadFile):
-    
-    return {"Uploaded filename": file.filename}
+    root_path = os.getcwd()
+    try:
+        file_path = f"{root_path}/{file.filename}"
+        with open(file_path, "wb") as f:
+            f.write(file.file.read())
+            return {"message":  "Filed saved successfully"}
+        return {"message": file_path}
+    except Exception as e:
+        return {"message": "Internal server error"}
