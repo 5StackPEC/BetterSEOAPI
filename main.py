5d82ef96ed8e49af0fed802a784a0d3ef9846cc0
CristianCazares <cm_cjavier@hotmail.com>
2024-05-29 15:02:47 -0600

    feat: modified boilerplate

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
+
+from uvicorn import run
 
 app = FastAPI()
 
+origins = ["*"]
+methods = ["*"]
+headers = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=methods,
+    allow_headers=headers,
+)
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Welcome to Better SEO API!"}
 
 
-@app.get("/score")
-def get_lighthouse_score():
-    return {"LighthouseScore": 100}
+@app.post("/score")
+async def get_score(file: Annotated[bytes, File()]):
+    print(type(file))
+    return {"file_size": len(file)}
+
+@app.post("/score_alt")
+async def get_score_alt(file: UploadFile):
+    
+    return {"Uploaded filename": file.filename}
